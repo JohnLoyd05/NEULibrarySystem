4f4e3452ed11a982d76b066908ad9701955fb240
JohnLoyd05 <johnloyd.patria@neu.edu.ph>
2026-03-15 02:03:25 +0800

    Delete InfoMan2-LibrarySystem directory

--- InfoMan2-LibrarySystem/backend (java spring boot)/BlockedEmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.neulibrarysystem.librarysystem;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlockedEmployeeRepository extends JpaRepository<BlockedEmployee, String> {
-    // Spring Boot automatically gives us the save() and existsById() methods
-}
