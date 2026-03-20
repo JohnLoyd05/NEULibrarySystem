f9acd28452b6bc504a171d3f1c42199803db78d0
JohnLoyd05 <johnloyd.patria@neu.edu.ph>
2026-03-20 20:50:24 +0800

    Delete Patria_InfoMan2_LibSys directory

--- Patria_InfoMan2_LibSys/backend (javaspringboot)/librarysystem/librarysystem/src/main/java/com/neulibrarysystem/librarysystem/BlockedStudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.neulibrarysystem.librarysystem;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlockedStudentRepository extends JpaRepository<BlockedStudent, String> {
-
-}
