728546fcbe4eb84d84cb7d5eb31c76c23ddc39b9
JohnLoyd05 <johnloyd.patria@neu.edu.ph>
2026-03-20 20:39:43 +0800

    Delete librarysystem directory

--- librarysystem/src/main/java/com/neulibrarysystem/librarysystem/AdminLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.neulibrarysystem.librarysystem;
-
-import com.neulibrarysystem.librarysystem.AdminLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminLogRepository extends JpaRepository<AdminLog, Long> {
-}
