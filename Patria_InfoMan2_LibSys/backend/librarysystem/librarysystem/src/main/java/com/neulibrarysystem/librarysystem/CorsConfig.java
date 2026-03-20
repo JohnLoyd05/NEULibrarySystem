9427372495e8152e237fd9b802f33c1e8b57be80
JohnLoyd05 <johnloyd.patria@neu.edu.ph>
2026-03-20 21:40:25 +0800

    Add files via upload

---
+++ Patria_InfoMan2_LibSys/backend/librarysystem/librarysystem/src/main/java/com/neulibrarysystem/librarysystem/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.neulibrarysystem.librarysystem;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5500",
+                        "http://localhost:3000",
+                        "http://unilibsysjlp05.infinityfreeapp.com"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
