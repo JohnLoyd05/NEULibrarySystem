6d7079688f2a272fe740bfb2875c42e916f7ec96
JohnLoyd05 <johnloyd.patria@neu.edu.ph>
2026-03-20 23:36:15 +0800

    Add files via upload

--- Patria_InfoMan2_LibSys/backend/librarysystem/librarysystem/src/main/java/com/neulibrarysystem/librarysystem/LibrarysystemApplication.java
+++ Patria_InfoMan2_LibSys/backend/librarysystem/librarysystem/src/main/java/com/neulibrarysystem/librarysystem/LibrarysystemApplication.java
@@ -2,9 +2,16 @@ package com.neulibrarysystem.librarysystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class LibrarysystemApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Manila"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibrarysystemApplication.class, args);
